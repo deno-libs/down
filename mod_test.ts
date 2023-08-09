55aadf98f5024b8f195ae1718d4adda2d4346665
v 1 r t l <hi@v1rtl.site>
2023-08-09 19:50:21 +0300

    Update mod_test.ts

--- mod_test.ts
+++ mod_test.ts
@@ -3,13 +3,13 @@ import { down } from './mod.ts'
 
 describe('down', () => {
   it('returns true if website is down', async () => {
-    const isDown = await down('v1rtl.site')
+    const isDown = await down('this.is.down.v1rtl.site')
 
     expect(isDown).toBe(true)
   })
   it('supports custom timeout', async () => {
     const t = performance.now()
-    const isDown = await down('v1rtl.site', 0.5)
+    const isDown = await down('this.is.down.v1rtl.site', 0.5)
     expect(performance.now() - t).toBeGreaterThan(500)
 
     expect(isDown).toBe(true)
